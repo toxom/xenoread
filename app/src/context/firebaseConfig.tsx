9942f092b4868eaa38c733be8c85c525cd3c9694
Janne Ruponen <ruponez@gmail.com>
2023-09-28 17:27:16 +0300

    buttons+firebase config

--- app/src/context/firebaseConfig.tsx
+++ app/src/context/firebaseConfig.tsx
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import * as admin from 'firebase-admin';
+// import * as admin from 'firebase-admin';
 
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { getAnalytics } from "firebase/analytics";
